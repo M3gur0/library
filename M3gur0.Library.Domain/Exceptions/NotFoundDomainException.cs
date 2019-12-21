2b5fccd5581598f5a8434f7a4e8525011f7a31e6
M3gur0 <jerome.bolot@gmail.com>
2019-12-21 22:30:31 +0100

    add new NotFoundException

---
+++ M3gur0.Library.Domain/Exceptions/NotFoundDomainException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace M3gur0.Library.Domain.Exceptions
+{
+    public class NotFoundDomainException : Exception
+    {
+        public NotFoundDomainException(string objectKey, Type objectType) : base($"There is no item [{objectType?.Name}] found with the following key {objectKey}") { }
+    }
+}
