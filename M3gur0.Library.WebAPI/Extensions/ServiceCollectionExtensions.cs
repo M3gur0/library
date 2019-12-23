2eb46c7bdc6867453fd3ed919457db9dfe9f5052
M3gur0 <jerome.bolot@gmail.com>
2019-12-23 18:47:21 +0100

    clean references

--- M3gur0.Library.WebAPI/Extensions/ServiceCollectionExtensions.cs
+++ M3gur0.Library.WebAPI/Extensions/ServiceCollectionExtensions.cs
@@ -5,12 +5,16 @@ using Hellang.Middleware.ProblemDetails;
 using M3gur0.Library.Domain;
 using M3gur0.Library.Domain.Exceptions;
 using M3gur0.Library.Infrastructure.Data.EF;
+using M3gur0.Library.Infrastructure.Events;
+using M3gur0.Library.Infrastructure.Events.InMemory;
+using M3gur0.Library.Infrastructure.Events.RabbitMQ;
 using M3gur0.Library.WebAPI.Modules;
 using M3gur0.Library.WebAPI.ProblemDetails;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using RabbitMQ.Client;
 using System;
 using System.Text.Json.Serialization;
 
@@ -33,6 +37,27 @@ namespace M3gur0.Library.WebAPI.Extensions
         public static IServiceCollection AddMediator<TMediatorAssembly>(this IServiceCollection services) => services.AddMediatR(typeof(TMediatorAssembly));
 
         public static IServiceCollection AddCustomMappers(this IServiceCollection services, params Type[] assemblyTypes) => services.AddAutoMapper(assemblyTypes);
+
+        public static IServiceCollection AddEventServices(this IServiceCollection services, string hostname, string brokerName, string scopeName, string subscriptionQueueName)
+        {
+            services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+            {
+                var connectionFactory = new ConnectionFactory { HostName = hostname, DispatchConsumersAsync = true };
+                return new DefaultRabbitMQPersistentConnection(connectionFactory);
+            });
+
+            services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
+            {
+                var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                var lifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                var subscriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+                return new EventBusRabbitMQ(rabbitMQPersistentConnection, subscriptionsManager, lifetimeScope, brokerName, scopeName, subscriptionQueueName);
+            });
+
+            services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
+
+            return services;
+        }
 
         public static IServiceCollection AddSwagger(this IServiceCollection services, OpenApiInfo openApiInfo) => services.AddSwaggerGen(options => options.SwaggerDoc(openApiInfo.Version, openApiInfo));
 
